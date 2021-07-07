f93789018908f645748f7fda5bb1e3a54c90345d
lakshits11 <lakshits11@gmail.com>
2021-07-07 10:36:37 +0530

    Added and modified some files

---
+++ src/components/HeroSection.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const HeroSection = () => {
+  return (
+    <div className="hero-container">
+      <video src="/videos/ocean.mp4" autoPlay loop muted />
+      <h1>Adventure Awaits !</h1>
+      <p>What are you waiting for ?</p>
+      <div className="hero-btns">
+        
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
